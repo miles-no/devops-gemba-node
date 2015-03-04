89ff8b2bccd96b083943338b0193eeb649f1d0b3
Jon Arild Torresdal <jon@torresdal.net>
2015-03-04 14:28:27 +0100

    Set default port to 80

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(process.env.PORT || 3000, function(){
+http.listen(80, function(){
   console.log('listening on *:3000');
 });
