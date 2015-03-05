5e3a00ad94faba8fa95c720e32353381ce5c6cbd
Jon Arild Torresdal <jon@torresdal.net>
2015-03-05 09:33:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  res.send('<h5>Server: ' + req.hostname + '</h5>')
 });
 
 http.listen(80, function(){
